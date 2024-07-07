c2a394886705ac63be0c2d65820207e606c8d1eb
Param Parikh <132662200+param2412@users.noreply.github.com>
2024-07-07 19:55:57 +0530

    Add files via upload
    It is a Real Time Character Counter that count the character length which we had enterd and it show the remaining character also.

---
+++ script.js
@@ -0,0 +1,11 @@
+const textareaE1 = document.getElementById("textarea");
+const totalCounterE1 = document.getElementById("total-counter");
+const remainingCounterE1 = document.getElementById("remaining-counter");
+textareaE1.addEventListener("keyup", ()=>{
+    upadateCounter();
+})
+upadateCounter()
+function upadateCounter(){
+    totalCounterE1.innerText = textareaE1.value.length;
+    remainingCounterE1.innerText = textareaE1.getAttribute("maxlength") - textareaE1.value.length;
+}
