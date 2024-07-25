6665b31aa0574bcc3627cefbb9717f8ac85868ee
Param Parikh <132662200+param2412@users.noreply.github.com>
2024-07-25 14:35:30 +0530

    script.js

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 const textareaE1 = document.getElementById("textarea");
 const totalCounterE1 = document.getElementById("total-counter");
 const remainingCounterE1 = document.getElementById("remaining-counter");
-textareaE1.addEventListener("keyup", ()=>{
+textareaE1.addEventListener("input", ()=>{
     upadateCounter();
 })
 upadateCounter()
